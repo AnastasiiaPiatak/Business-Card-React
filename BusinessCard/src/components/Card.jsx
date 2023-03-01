bdba23796da36fe29da9acfbb4cff4415460cc1d
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-03-01 20:44:31 +0200

    Changed image path

--- BusinessCard/src/components/Card.jsx
+++ BusinessCard/src/components/Card.jsx
@@ -2,23 +2,18 @@ import About from "./About";
 import Interests from "./Interests";
 import MainInfo from "./MainInfo";
 import SocialLinks from "./SocialLinks";
+import photo from "./images/Photo.jpg";
 
 export default function Card() {
   return (
     <>
       <div className="card">
-        <img
-          src="src/images/Photo.jpg"
-          alt="Anastasiia Piatak photo"
-          className="card__img"
-        />
-        <MainInfo/>
+        <img src={photo} alt="Anastasiia Piatak photo" className="card__img" />
+        <MainInfo />
         <About />
         <Interests />
         <SocialLinks />
-       </div>
-       
-      
+      </div>
     </>
   );
 }
