2836e733bf7464bc0e3a153513f1758fd1538ebf
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-03-01 16:41:44 +0200

    Added a social links section

--- Buisness Card/business_card-react/src/components/SocialLinks.jsx
+++ Buisness Card/business_card-react/src/components/SocialLinks.jsx
@@ -1,3 +1,29 @@
 export default function SocialLinks() {
-  return <h1>SocialLinks</h1>;
+  return (
+    <div className="socialLinks">
+      <span>
+        <a href="/" target="_blank">
+          <i className="fa-brands fa-square-twitter"></i>
+        </a>
+      </span>
+      <span>
+        <a href="https://www.facebook.com/anastasia.piatak" target="_blank">
+          <i className="fa-brands fa-square-facebook"></i>
+        </a>
+      </span>
+      <span>
+        <a
+          href="https://www.instagram.com/invites/contact/?i=358tukace46n&utm_content=2p0quyr"
+          target="_blank"
+        >
+          <i className="fa-brands fa-square-instagram"></i>
+        </a>
+      </span>
+      <span>
+        <a href="https://github.com/AnastasiiaPiatak" target="_blank">
+          <i className="fa-brands fa-square-github"></i>
+        </a>
+      </span>
+    </div>
+  );
 }
