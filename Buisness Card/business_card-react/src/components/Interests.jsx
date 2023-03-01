0a11b32e38cc4de644830ab423f68948b80ff93e
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2023-03-01 15:57:10 +0200

    Added an Interests section

--- Buisness Card/business_card-react/src/components/Interests.jsx
+++ Buisness Card/business_card-react/src/components/Interests.jsx
@@ -1,3 +1,11 @@
 export default function Interests() {
-  return <h1>Interests</h1>;
+  return (
+    <div className="interests">
+      <h2>Interests</h2>
+      <p>
+        Psychologist. Musician. Reader. Internet fanatic. Food lover. Crocheter.
+        Travel geek. Tea fanatic.
+      </p>
+    </div>
+  );
 }
